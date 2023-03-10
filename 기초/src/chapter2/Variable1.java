01597127f94073bf4d823c8e112856ca99461c77
Kyung Eun Park <dolgoraen513@naver.com>
2023-03-11 03:17:43 +0900

    second! 03/11

---
+++ 기초/src/chapter2/Variable1.java
@@ -0,0 +1,12 @@
+package chapter2;
+
+public class Variable1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age;	// 정수형 변수 age을 선언
+		age = 10;	// age 변수에 값 10을 대입
+		System.out.println(age);	// age 값 출력
+	}
+
+}
